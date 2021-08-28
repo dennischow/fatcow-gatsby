0a73b39b67e1f1c938e5244e589332660981bf59
Dennis Chow <dennischow@Denniss-MBP>
2021-08-28 11:50:51 +0800

    Style code updates

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,9 +6,38 @@ import * as constant from "./../app.constant";
 import Layout from "../components/layout/layout";
 import Seo from "../components/seo";
 
+import imgFeatureBannerBackground from "./../assets/images/home/polygon-colored-crossed-dark-extend-bg.jpg";
+
 const IndexPage = () => (
     <Layout>
         <div className="page-landing layout-page">
+
+            {/* Feature Banner Starts */}
+            <section className="feature-banner feature-banner--hero">
+                <div className="feature-banner__container c-grid-container">
+
+                    <div className="feature-banner__box">
+                        <div className="feature-banner__content">
+                            <h2 className="feature-banner__heading">
+                                <span className="feature-banner__begin-word feature-banner__stacker">Just A</span>
+                                <span className="feature-banner__stacker">Front-End</span>
+                                <span className="feature-banner__stacker">Web Developer</span>
+                                <span className="c-ninja-content"> Dennis Chow a.k.a. Fat-Cow</span>
+                            </h2>
+                            <p className="feature-banner__sub-heading">Who cares about things that users see and interact with</p>
+                        </div>
+                        <div className="feature-banner__visual-content">
+                            <div className="feature-banner__visual"
+                                role="img"
+                                aria-label="picture of a low-poly illustraion artwork by dennis chow"
+                                style={{ backgroundImage: `url(${imgFeatureBannerBackground})` }}>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </section>
+            {/* Feature Banner Ends */}
 
             <div className="greeting">
                 <div className="c-grid-container">
