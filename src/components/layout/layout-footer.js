b679d53357d65573a84e71512e8ea3c04317fd0e
Dennis Chow <dennischow@Denniss-MBP>
2021-08-28 23:56:19 +0800

    Remove thing in footer

--- src/components/layout/layout-footer.js
+++ src/components/layout/layout-footer.js
@@ -7,7 +7,6 @@ const LayoutFooter = () => (
         <div className="layout-footer__container c-grid-container">
             <div className="layout-footer__general">
                 <span>© {new Date().getFullYear()} Fat-Cow Production</span>
-                <span>Deployed time: <strong>{new Date()}</strong></span>
                 <span>
                     Handcrafted by <Link className="layout-footer__link" to={constant._ROUTES_.HOME}>Dennis Chow</Link>
                 </span>
